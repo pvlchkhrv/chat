a9f7db3c685019089f7fe346c9f080464aecbba3
PavelChukharev <pchukharev@kapitus.com>
2025-10-25 08:57:59 +0400

    init messages

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MessageService } from '../shared/data-access/message.service';
+import { MessageList } from './ui/message-list';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MessageList
+  ],
   template: `
-    <p>
-      home works!
-    </p>
+    <div class="container">
+      <app-message-list [messages]="messageService.messages()" />
+    </div>
   `,
   styles: ``,
 })
 export default class Home {
-
+  readonly messageService = inject(MessageService);
 }
