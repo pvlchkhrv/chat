042bc4dc62d7736966a719706391cb6d6b5c822f
PavelChukharev <pchukharev@kapitus.com>
2025-10-25 07:59:50 +0400

    configure firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,45 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  InjectionToken,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import { connectAuthEmulator, getAuth } from 'firebase/auth';
+import { connectFirestoreEmulator, Firestore, getFirestore, initializeFirestore, } from 'firebase/firestore';
+import { environment } from '../environments/environment';
 
 import { routes } from './app.routes';
 
+const app = initializeApp(environment.firebase);
+
+export const AUTH = new InjectionToken('Firebase auth', {
+  providedIn: 'root',
+  factory: () => {
+    const auth = getAuth();
+    if (environment.useEmulators) {
+      connectAuthEmulator(auth, 'http://localhost:9099', {
+        disableWarnings: true,
+      });
+    }
+    return auth;
+  },
+});
+
+export const FIRESTORE = new InjectionToken('Firebase firestore', {
+  providedIn: 'root',
+  factory: () => {
+    let firestore: Firestore;
+    if (environment.useEmulators) {
+      firestore = initializeFirestore(app, {});
+      connectFirestoreEmulator(firestore, 'localhost', 8080);
+    } else {
+      firestore = getFirestore();
+    }
+    return firestore;
+  },
+});
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDDzsNzPfkneJmw0b4Xp85_zs2eBZVAC08",
+    authDomain: "chat-morony.firebaseapp.com",
+    projectId: "chat-morony",
+    storageBucket: "chat-morony.firebasestorage.app",
+    messagingSenderId: "947726135643",
+    appId: "1:947726135643:web:4d38016ac5fbf15dc9ea7e"
+  },
+  production: true,
+  useEmulators: false,
+};
